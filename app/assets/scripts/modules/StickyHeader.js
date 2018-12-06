7d2213114cfc3a5d9116d769e2729602b8b11b47
Christian <mikkel.boesen.dalsgaard@gmail.com>
2018-12-06 14:56:20 +0100

    Completed sticky header functionality.

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,67 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smothScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+  constructor() {
+    this.siteHeader = $(".site-header");
+    this.headerTriggerElement = $(".large-hero__title");
+    this.createHeaderWaypoint();
+    this.pageSections = $(".page-section");
+    this.headerLinks = $(".primary_nav a")
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
+  }
+
+  createHeaderWaypoint(){
+    var that = this; /* Waypoint detroys the class' "this keyword", therefore
+                          the keyword has to be saved in the beginning of the function in a variable "that"*/
+    new Waypoint({
+      element: this.headerTriggerElement[0],/*Waypoint expects a javascript DOM element and not a Jquery object,
+      first element in the jquery object is a pointer to a the javascript DOM element*/
+      handler: function(direction) {
+        //console.log(this);
+        if(direction == "down"){
+        that.siteHeader.addClass("site-header--dark");
+        }else {
+        that.siteHeader.removeClass("site-header--dark");
+      }
+      }
+    });
+  }
+
+createPageSectionWaypoints(){
+  var that = this;
+  this.pageSections.each(function(){
+  var currentPageSection = this;
+    new Waypoint({
+      element: currentPageSection,
+      handler: function(direction){
+        if(direction == "down"){
+          var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+          that.headerLinks.removeClass("is-current-link");
+          $(matchingHeaderLink).addClass("is-current-link");
+        }
+      },
+        offset: "18%"
+    });
+    new Waypoint({
+      element: currentPageSection,
+      handler: function(direction){
+        if(direction == "up"){
+          var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+          that.headerLinks.removeClass("is-current-link");
+          $(matchingHeaderLink).addClass("is-current-link");
+        }
+      },
+        offset: "-40%"
+    });
+  });
+}
+}
+
+export default StickyHeader;
