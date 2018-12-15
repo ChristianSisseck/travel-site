ebdd3657e9b9728c4ae1b955199703e6de5532c9
Christian <mikkel.boesen.dalsgaard@gmail.com>
2018-12-15 10:46:58 +0100

    Added browser supports fallbacks.

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,1 +1,8 @@
-import 'lazysizes';
+// The vendor.js file is only used to extern JavaScript libaries that doesn't change much.
+// It is bestpractice to have a vendor.js file.
+//Vendor -> a person or company offering something for sale, especially a trader in the street. Or a external javascript
+// file that offer something extra to a program
+
+import "../../temp/scripts/modernizr";
+import 'picturefill'; // installed picture fill with NPM - will help old browsers that doesn't support the picture and srcset tag.
+import 'lazysizes'; // installed to make picture load only when scrolled to.
